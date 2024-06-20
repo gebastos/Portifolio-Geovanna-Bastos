c42c8694f679c1c5194cb21d7e169873249f6e2d
geovanna <167234198+gebastos@users.noreply.github.com>
2024-06-20 17:42:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -64,3 +64,16 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             }
         });
     });
+
+    // SOCIAL
+    const buttonsComponent = document.querySelector('.buttons');
+const buttonsToggle = document.querySelector('.buttons__toggle');
+
+buttonsToggle.addEventListener('click', toggleButtons);
+
+function toggleButtons() {
+	buttonsToggle.classList.toggle('buttons__toggle--active');
+	buttonsComponent.classList.toggle('buttons--active');
+	document.activeElement.blur();
+}
+
